ad29f2f09fad430736572cdd1482940072181310
jma-droid <johnmariojb08@gmail.com>
2025-09-16 23:14:09 -0500

    Add files via upload

---
+++ Cod. op. M.py
@@ -0,0 +1,27 @@
+#Cod. op. M
+import tensorflow as tf
+
+# Declarar las matrices
+X = tf.constant([[-1, 3, 4],
+                 [ 2, 6, 4],
+                 [-3, 1, 5]], dtype=tf.float32)
+
+Y = tf.constant([[ 2, -5, 4],
+                 [ 1,  2, 6],
+                 [ 3, -1, 5]], dtype=tf.float32)
+
+# Multiplicación de matrices
+W = tf.linalg.matmul(X, Y)
+
+# Inversa y determinante de X
+Z = tf.linalg.inv(X)
+V = tf.linalg.det(X)
+
+#Confirmar inversa
+I = tf.linalg.matmul(X,Z)
+
+# Mostrar resultados
+print("Resultado de X * Y:\n", W.numpy())
+print("\nInversa de X:\n", Z)
+print("\nDeterminante de X:\n", V)
+print("\nMatriz X*X^-1(Z):\n", I)
